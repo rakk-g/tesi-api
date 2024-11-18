0ebe35bd4d789d2ac69250cfd06e9ab4e84c36af
Rakk <rachele@logorroici.org>
2024-11-18 19:28:43 +0100

    Giusto prima di rifattorizzare exp A2 in Khoury2011

--- simulaz/kh11expA2.m
+++ simulaz/kh11expA2.m
@@ -4,8 +4,8 @@
 ## Prende: niente
 ##
 ## Come funziona:
-## - le c.i. sono scelte sempre da una UNIFORME su [xmin, xmax] e analog. su y.
-## - si esplora lo spazio parametrico SOLO in m, cogli altri valori tenuti costanti.
+## - le c.i. si cicla la popTot, viene scelta random la frazione per ogni comparto.
+## - si esplora lo spazio parametrico in w ed m.
 ##
 ## For each simulation, format is # t, H, F.
 ## Adds a terminating line with parameters, calculated values and additional data:
@@ -19,67 +19,83 @@
 ##
 
 # number of simulations (resolution on m)
-N = 73;
+N = 36 ;
 # number of c.i. for each simulation
-Nci = 400;
+Nci = 24 ;
+# number of cycles on w
+Nwc = 12 ;
+# number of cycles on total population
+Npop = 6 ;
 # number of days to run per each simulations
-nDays= 900;
+nDays= 365;
 
 
-# UNIFORM SPACE for initial conditions
-xmin=1;
-xmax=12000;
-ymin=1;
-ymax=6000;
+# condizioni iniziali: random, scelgo solo la pop.
+popMin = 4     ;
+popMax = 15000 ;
+mMin   = 0.006 ;
+mMax   = 0.8  ;
+wMin   = 3000  ;
+wMax   = 30000 :
 
-# parameters
-w=27000 ;
+
+# FIXED parameters
 L=2500 ;
 alpha=0.25 ;
 sigma=0.75 ;
-mMin= 0.006 ;
-mMax= 0.85  ;
+
 
 g = @(y) ( y(1)+y(2) );
 for in = 0:N
-    # set m
-    m= mMin + (in/N)*(mMax -mMin)
+  # set m
+  m= mMin + (in/N)*(mMax -mMin) ;
 
-    df = @(t,y) [ ( L*g(y)/( w + g(y) ) - y(1)*(alpha -sigma*( y(2)/g(y) ) ) ) ;
-    ( y(1)*(alpha -sigma*( y(2)/g(y) ) ) -m*y(2) ) ] ;
+    for iw=0:Nwc
+      # set w
+      w = wMin + (iw/Nwc)*(wMax-wMin) ;
 
-  for cix = 1:Nci
-    x0=(xmax-xmin)*rand() +xmin ;
-    y0=(ymax-ymin)*rand() +ymin ;
-    [t,y] = ode45(df, [0:1:nDays], [x0, y0]);
-    res = [t,y];
+      for p=0:Npop
+        popTot = popMin + (p/Npop)*(popMax-popMin) ;
 
-    prefix= "output/";
-    faileName = sprintf( "kh11ExpA2-%f.dat", now() );
-    faileName = [ prefix, faileName ];
-    faile = fopen( faileName, "w" );
-    fprintf( faile, "# t, H, F\n");
-    # fprintf( faile, "# parameters: %f \t %f \t %f \t %f \t %f\n", w, L, alpha, sigma, m );
-    for ir=1:size( res, 1 )
-      fprintf( faile, "%f \t %f \t %f \n", res(ir, :) );
-    endfor
-    ## terminating line with parameters, calculated values and additional data:
-    fprintf( faile, "\n" );
-    fprintf( faile, "# w, L, alpha, sigma, m, lhs(cond6b), m1*, m2*, m3*, initial pop., final pop.\n" );
-    cond6bVal = alpha - L/w;
-    mStarFac = L/(2*w);
-    mStarRad = (alpha - sigma)^2 +4 * sigma * L / w;
-    m1 = mStarFac* ( alpha + sigma - sqrt( mStarRad ) ) / cond6bVal;
-    m2 = mStarFac* ( alpha + sigma + sqrt( mStarRad ) ) / cond6bVal;
-    m1Star = min( m1, m2 );
-    m2Star = max( m1, m2 );
-    m3Star = sigma*L / (w*(alpha+sigma));
-    initialPop = res( 1 , 2 ) + res( 1 , 3 );
-    finalPop = res( size(res,1) , 2 ) + res( size(res,1) , 3 );
-    fprintf( faile, "%f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \n",
-        [ w, L, alpha, sigma, m, cond6bVal, m1Star, m2Star, m3Star, initialPop, finalPop ] );
-    fclose(faile);
-  endfor
+        df = @(t,y) [ ( L*g(y)/( w + g(y) ) - y(1)*(alpha -sigma*( y(2)/g(y) ) ) ) ;
+        ( y(1)*(alpha -sigma*( y(2)/g(y) ) ) -m*y(2) ) ] ;
 
-endfor
+        cas = rand();
+        x0=(xmax-xmin)*cas +xmin ;
+        y0=(ymax-ymin)*(1-cas) +ymin ;
+        [t,y] = ode45(df, [0:1:nDays], [x0, y0]);
+        res = [t,y];
+
+        prefix= "output/";
+        faileName = sprintf( "kh11ExpA2-%f.dat", now() );
+        faileName = [ prefix, faileName ];
+        faile = fopen( faileName, "w" );
+        fprintf( faile, "# t, H, F\n");
+        # fprintf( faile, "# parameters: %f \t %f \t %f \t %f \t %f\n", w, L, alpha, sigma, m );
+        for ir=1:size( res, 1 )
+          fprintf( faile, "%f \t %f \t %f \n", res(ir, :) );
+        endfor
+        ## terminating line with parameters, calculated values and additional data:
+        fprintf( faile, "\n" );
+        fprintf( faile, "# w, L, alpha, sigma, m, lhs(cond6b), m1*, m2*, m3*, initial pop., final pop.\n" );
+        cond6bVal = alpha - L/w;
+        mStarFac = L/(2*w);
+        mStarRad = (alpha - sigma)^2 +4 * sigma * L / w;
+        m1 = mStarFac* ( alpha + sigma - sqrt( mStarRad ) ) / cond6bVal;
+        m2 = mStarFac* ( alpha + sigma + sqrt( mStarRad ) ) / cond6bVal;
+        m1Star = min( m1, m2 );
+        m2Star = max( m1, m2 );
+        m3Star = sigma*L / (w*(alpha+sigma));
+        initialPop = res( 1 , 2 ) + res( 1 , 3 );
+        finalPop = res( size(res,1) , 2 ) + res( size(res,1) , 3 );
+        fprintf( faile, "%f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \n",
+            [ w, L, alpha, sigma, m, cond6bVal, m1Star, m2Star, m3Star, initialPop, finalPop ] );
+
+        printf( faile, "%f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \t %f \n",
+            [ w, L, alpha, sigma, m, cond6bVal, m1Star, m2Star, m3Star, initialPop, finalPop ] );
+
+        fclose(faile);
+      endfor # popTot
+  endfor # w
+endfor # m
 
